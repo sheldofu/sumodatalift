06295d570df536614745f1fd04c84665821b9ebc
Sheldon <thesheldon@googlemail.com>
2021-04-25 02:10:47 +0200

    progress

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests;
 import json;
 import pprint;
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 
 pp = pprint.PrettyPrinter()
 URL = 'https://en.wikipedia.org/wiki/Tochinoshin_Tsuyoshi'
@@ -11,14 +11,50 @@ soup = BeautifulSoup(page.content, 'html.parser')
 recordTable = soup.find('table', class_='wikitable')
 topRow = recordTable.find('tr')
 monthsRow = topRow.find_all('th')
-monthList = []
-for months in monthsRow:
-    bler = months
-    pp.pprint(bler)
-    monthList.append(bler)
-# pp.pprint(monthList)
 tournamentRecord = []
-tournamentRecord.append({'tournament year': 2006, 'tournament month': 'may', 'wins': 9, 'losses': 6})
+monthList = []
+locationList = []
+yearList = []
+rankList = []
+recordList = []
+
+for months in monthsRow:
+    spanSelection = months.find('span')
+    if spanSelection == None:
+        continue
+    spanSaved = spanSelection.extract()
+    location = spanSaved.find('a')
+    locationList.append(location.text)
+    monthList.append(months.text)
+# pp.pprint(monthList)
+# pp.pprint(locationList)
+
+for resultsRow in topRow.next_siblings:
+    year = resultsRow.find('th')
+    if isinstance(year , int) or year == None:
+        continue
+    # print(year.text)
+    yearList.append(year.text)
+    tournamentCells = resultsRow.find_all('td')
+    # pp.pprint(tournamentCells)
+    for tournamentCell in tournamentCells:
+        tournament = tournamentCell.find('span')
+        if tournament == None:
+            continue
+        # rankList.append(tournament.text)
+        record = tournament.next_sibling.next_sibling
+        if isinstance(record, NavigableString):
+            continue
+        pp.pprint(record.text)
+        recordList.append({'tournament':tournament.text,'record':record.text})
+
+pp.pprint(rankList)
+
+
+for y in yearList:
+    for m in monthList:
+        for r in recordList:
+            tournamentRecord.append({'tournament year': y, 'tournament month': m, 'record': r })
 
 with open('sumo.json', 'w') as outsumo:
     json.dump(tournamentRecord, outsumo)
